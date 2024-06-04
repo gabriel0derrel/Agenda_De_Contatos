0cc07cb3fb6244d34619010bcfb4a52e3c7cfcda
gabriel0derrel <125845359+gabriel0derrel@users.noreply.github.com>
2024-06-04 08:29:27 -0300

    Add files via upload

---
+++ AgendaDeContatos/src/modelos/principais/Icrud.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos.principais;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author misuka
+ */
+public interface Icrud {
+    void incluir(Contato objeto) throws Exception;
+    void excluir(String nome) throws Exception;
+    void alterar(Contato objeto) throws Exception;
+    ArrayList<Contato> consultar(Contato filtro) throws Exception;
+    ArrayList<Contato> listar() throws Exception;
+}
